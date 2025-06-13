857ae4537f320247a87309d80176b4a91ed8902e
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-06-13 16:51:27 +0530

    Condition added

--- conversion/string-to-num.js
+++ conversion/string-to-num.js
@@ -1,2 +1,10 @@
 const userinput="42"//As string
 const numberInput=num(userinput)//changed string to number
+
+if(!isNaN(numberInput))
+{
+    console.log(numberInput+8)
+}
+else{
+    console.log("Invalid Input")
+}
