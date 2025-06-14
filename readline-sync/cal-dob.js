9443fe42be71ae0761659895cda87403beaf28bf
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-06-14 12:39:07 +0530

    Input for age added

--- readline-sync/cal-dob.js
+++ readline-sync/cal-dob.js
@@ -1,3 +1,5 @@
 const readlineSync=require('readline-Sync');//name of module imported (require) load the module
 const username=readlineSync.question('May I know your name?');
 console.log('Welcome, ${username}!');
+
+const userage=readlineSync.question('May I know your age?');
