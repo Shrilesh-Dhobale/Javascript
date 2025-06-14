76b9e5cc653bf2aa74a077fd6a87cc8cf5f95efa
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-06-14 12:37:03 +0530

    Input section added

--- readline-sync/cal-dob.js
+++ readline-sync/cal-dob.js
@@ -1,1 +1,3 @@
 const readlineSync=require('readline-Sync');//name of module imported (require) load the module
+const username=readlineSync.question('May I know your name?');
+console.log('Welcome, ${username}!');
